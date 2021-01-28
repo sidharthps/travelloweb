a18e6a87df8ed3e9c646999f8da02cbff0a897ce
Sidharth <sidharthpsnirmallur@gmail.com>
2021-01-28 18:35:01 -0500

    Special offer updated for Chennai only

--- travello/models.py
+++ travello/models.py
@@ -7,3 +7,4 @@ class Destination:
     image : str
     destination : str
     price : int
+    offer : bool
--- travello/views.py
+++ travello/views.py
@@ -9,18 +9,21 @@ def index(request):
     dest1.description = 'The city never sleeps'
     dest1.price = 700
     dest1.image = 'destination_1.jpg'
+    dest1.offer = False
 
     dest2 = Destination()
     dest2.name = 'Chennai'
     dest2.description = 'Developing City'
     dest2.price = 900
     dest2.image = 'destination_2.jpg'
+    dest2.offer = True
 
     dest3 = Destination()
     dest3.name = 'Hyderabad'
     dest3.description = 'Ramoji Rao Film City'
     dest3.price = 1000
     dest3.image = 'destination_3.jpg'
+    dest3.offer = False
 
     dests = [dest1,dest2,dest3]
 
