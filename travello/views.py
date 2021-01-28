44b2ab28a2bc790ef86465dc3bc7d9c27778e557
Sidharth <sidharthpsnirmallur@gmail.com>
2021-01-28 18:19:52 -0500

    Images updated

--- travello/views.py
+++ travello/views.py
@@ -8,18 +8,21 @@ def index(request):
     dest1.name = 'Mumbai'
     dest1.description = 'The city never sleeps'
     dest1.price = 700
+    dest1.image = 'destination_1.jpg'
 
     dest2 = Destination()
     dest2.name = 'Chennai'
     dest2.description = 'Developing City'
     dest2.price = 900
+    dest2.image = 'destination_2.jpg'
 
     dest3 = Destination()
     dest3.name = 'Hyderabad'
     dest3.description = 'Ramoji Rao Film City'
     dest3.price = 1000
+    dest3.image = 'destination_3.jpg'
+
+    dests = [dest1,dest2,dest3]
 
 
-    return render(request,"index.html",{'dest1':dest1,
-                                        'dest2':dest2,
-                                        'dest3':dest3})
+    return render(request,"index.html",{'dests':dests})
