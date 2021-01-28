a2e6db61d08f6272af8b28db31bb22436e50bc8c
Sidharth <sidharthpsnirmallur@gmail.com>
2021-01-28 16:58:33 -0500

    Places decreced to three and destination changed

--- travello/models.py
+++ travello/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Destination:
+    id : int
+    name: str
+    image : str
+    destination : str
+    price : int
--- travello/views.py
+++ travello/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
-
+from . models import Destination
 # Create your views here.
 
 def index(request):
-    return render(request,"index.html")
+
+    dest1 = Destination()
+    dest1.name = 'Mumbai'
+    dest1.description = 'The city never sleeps'
+    dest1.price = 700
+
+    dest2 = Destination()
+    dest2.name = 'Chennai'
+    dest2.description = 'Developing City'
+    dest2.price = 900
+
+    dest3 = Destination()
+    dest3.name = 'Hyderabad'
+    dest3.description = 'Ramoji Rao Film City'
+    dest3.price = 1000
+
+
+    return render(request,"index.html",{'dest1':dest1,
+                                        'dest2':dest2,
+                                        'dest3':dest3})
