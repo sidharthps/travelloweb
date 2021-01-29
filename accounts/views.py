1dc6f64b9127b552644f6fc763f58b5494088d23
Sidharth <sidharthpsnirmallur@gmail.com>
2021-01-29 13:01:57 -0500

    Errors fixed in user registration

--- accounts/views.py
+++ accounts/views.py
@@ -10,22 +10,34 @@ def register(request):
         password1 = request.POST['password1']
         password2 = request.POST['password2']
         email = request.POST['email']
-        if password1 == password2:
+        if password1==password2:
             if User.objects.filter(username=username).exists():
                 print('Username taken')
             elif User.objects.filter(email=email).exists():
                 print('Email taken')
             else:
-                user = User.objects.create_user(username=username,
-                                                password=password1,
-                                                email=email,
-                                                first_name=first_name,
-                                                last_name=last_name)
+                user = User.objects.create_user(username=username, password=password1, email=email,
+                                                first_name=first_name, last_name=last_name)
                 user.save();
                 print('user created')
-
-
-
         else:
+            print('password not matching')
             return redirect('/')
+    else:
+        return render(request, 'register.html')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
