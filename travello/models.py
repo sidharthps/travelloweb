de6c0957fff6586f9a1833fbc95ed0bebd57e3e3
Sidharth <sidharthpsnirmallur@gmail.com>
2021-01-29 09:59:35 -0500

    Destination admin added

--- travello/models.py
+++ travello/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Destination(models.Model):
-    name: models.CharField(max_length=100)
-    image : models.ImageField(upload_to='pics')
-    destination : models.TextField()
-    price : models.IntegerField()
-    offer : models.BooleanField(default=False)
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='pics')
+    destination = models.TextField(max_length=100)
+    price = models.IntegerField()
+    offer = models.BooleanField(default=False)
