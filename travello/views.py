0d8ceff1f2e5bb0fb523138d28e0472db9cec482
Sidharth <sidharthpsnirmallur@gmail.com>
2021-01-29 12:23:45 -0500

    User registration added

--- travello/views.py
+++ travello/views.py
@@ -3,29 +3,10 @@ from . models import Destination
 # Create your views here.
 
 def index(request):
-
-    dest1 = Destination()
-    dest1.name = 'Mumbai'
-    dest1.description = 'The city never sleeps'
-    dest1.price = 700
-    dest1.image = 'destination_1.jpg'
-    dest1.offer = False
-
-    dest2 = Destination()
-    dest2.name = 'Chennai'
-    dest2.description = 'Developing City'
-    dest2.price = 900
-    dest2.image = 'destination_2.jpg'
-    dest2.offer = True
-
-    dest3 = Destination()
-    dest3.name = 'Hyderabad'
-    dest3.description = 'Ramoji Rao Film City'
-    dest3.price = 1000
-    dest3.image = 'destination_3.jpg'
-    dest3.offer = False
-
-    dests = [dest1,dest2,dest3]
+    dests = Destination.objects.all()
+    return render(request, "index.html", {'dests': dests})
 
 
-    return render(request,"index.html",{'dests':dests})
+
+
+
