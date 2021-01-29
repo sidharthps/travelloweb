a62e62487b07aff23c5358ece73c62ce8157c747
Sidharth <sidharthpsnirmallur@gmail.com>
2021-01-29 14:22:38 -0500

    Login added

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,20 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.models import User,auth
 from django.contrib import messages
 
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username,password=password)
+        if user is not None:
+            auth.login(request,user)
+            return redirect("/")
+        else:
+            messages.info(request,'Invalid Credentials')
+            return redirect('login')
+    else:
+        return render(request,'login.html')
+
 
 def register(request):
     if request.method == 'POST':
@@ -23,6 +37,7 @@ def register(request):
                                                 first_name=first_name, last_name=last_name)
                 user.save();
                 print('user created')
+                return redirect('login')
         else:
             messages.info(request,'Passwords not matching')
             return redirect('register')
